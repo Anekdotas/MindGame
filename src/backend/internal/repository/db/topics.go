b1ef61c6b5bd7ad02c5b854d7c83017c819c5262
Bohdan Borysei <borysej90@gmail.com>
2021-10-20 00:05:17 +0300

    Added repository method for topic creation

--- src/backend/internal/repository/db/topics.go
+++ src/backend/internal/repository/db/topics.go
@@ -10,7 +10,7 @@ type TopicRecord struct {
 	ID               int    `db:"id"`
 	Name             string `db:"name"`
 	Author           string `db:"author"`
-	QuestionsPerGame int    `db:"question_per_game"`
+	QuestionsPerGame int    `db:"questions_per_game"`
 }
 
 const TopicsTableName = "topics"
@@ -27,4 +27,27 @@ func (r *Repo) GetTopics(ctx context.Context) ([]*anekdotas.Topic, error) {
 			Name:   record.Name,
 		}
 	}, records), nil
+}
+
+func (r *Repo) CreateTopic(ctx context.Context, topic *anekdotas.Topic) (name string, err error) {
+	stmt := fmt.Sprintf(
+		`INSERT INTO %s
+		(name, author, questions_per_game)
+		VALUES (
+			:name,
+			:author,
+			:questions_per_game
+		) RETURNING name`,
+		TopicsTableName,
+	)
+	record := &TopicRecord{
+		Name:             topic.Name,
+		Author:           topic.Author,
+		// TODO: AN-36 - use actual questions_per_game
+		QuestionsPerGame: 10,
+	}
+	if _, err = r.db.NamedExecContext(ctx, stmt, record); err != nil {
+		return
+	}
+	return topic.Name, nil
 }
