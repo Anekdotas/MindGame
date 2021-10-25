23b1cbe638d964cca6873085422855396a62ae93
Bohdan Borysei <borysej90@gmail.com>
2021-10-25 21:44:52 +0300

    Created method for saving media files

--- src/backend/internal/logic/logic.go
+++ src/backend/internal/logic/logic.go
@@ -4,10 +4,16 @@ import (
 	"anekdotas"
 	"anekdotas/internal/repository"
 	"context"
+	"fmt"
+	"io"
+	"os"
+	"strings"
 )
 
 type Logic struct {
-	repo repository.Repository
+	repo           repository.Repository
+	mediaDir   string
+	hostPrefix string
 }
 
 func New(repo repository.Repository) *Logic {
@@ -20,6 +26,31 @@ func (l *Logic) GetQuestionsByTopic(ctx context.Context, topic string) ([]*anekd
 
 func (l *Logic) CreateQuestion(ctx context.Context, topic string, question *anekdotas.Question) (id int64, err error) {
 	return l.repo.CreateQuestion(ctx, topic, question)
+}
+
+func (l *Logic) SaveMediaFile(ctx context.Context, questionID int64, filename string, src io.Reader) error {
+	extension := ""
+	if split := strings.Split(filename, "."); len(split) > 0 {
+		extension = split[len(split)-1]
+	}
+	dst, err := os.CreateTemp(l.mediaDir, fmt.Sprintf("*.%s", extension))
+	if err != nil {
+		return err
+	}
+	defer dst.Close()
+	written, err := io.Copy(dst, src)
+	if err != nil {
+		return err
+	}
+	// If nothing was written eventually, then remove newly created empty file
+	if written == 0 {
+		if err := os.Remove(dst.Name()); err != nil {
+			return err
+		}
+	}
+	mediaURL := strings.TrimPrefix(dst.Name(), l.mediaDir)
+	mediaURL = fmt.Sprintf("%s/media%s", l.hostPrefix, mediaURL)
+	return l.repo.UpdateMediaURL(ctx, questionID, mediaURL)
 }
 
 func (l *Logic) GetAllTopics(ctx context.Context) ([]*anekdotas.Topic, error) {
