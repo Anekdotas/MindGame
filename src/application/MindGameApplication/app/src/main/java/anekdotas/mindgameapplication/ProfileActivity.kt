08441af2ea7c9bb564afdb93255d7d57009170c3
Andrej Jegorov <andrej.jegorov24@gmail.com>
2022-01-14 21:29:18 +0200

    improved ListView to display statistics

--- src/application/MindGameApplication/app/src/main/java/anekdotas/mindgameapplication/ProfileActivity.kt
+++ src/application/MindGameApplication/app/src/main/java/anekdotas/mindgameapplication/ProfileActivity.kt
@@ -5,12 +5,15 @@ import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.view.View
 import android.widget.ArrayAdapter
-import anekdotas.mindgameapplication.databinding.ActivityListTopicsBinding
 import anekdotas.mindgameapplication.databinding.ActivityProfileBinding
+import anekdotas.mindgameapplication.java.StatisticsAdapter
+import anekdotas.mindgameapplication.java.StatisticsRecord
 import java.util.ArrayList
 
 class ProfileActivity : AppCompatActivity() {
     private lateinit var binding : ActivityProfileBinding
+    var statisticsList =  ArrayList<StatisticsRecord>()
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
 
@@ -20,18 +23,30 @@ class ProfileActivity : AppCompatActivity() {
 
         actionBarSetup()
 
-        fillStatistics()
+//        fillStatistics()
 
+        setStatistics()
     }
 
-    private fun fillStatistics() {
-        var statistics = arrayOf("Coins gained: 500", "Coins spent: 480", "Games played: 32", "Best strike: 7", "Total time spent: 35h", "Topics created: 0", "Average time per topic: 5 min", "Topics played: 80", "Topics rated: 77")
-        val adapter: ArrayAdapter<String> = ArrayAdapter<String>(
-            this,
-            android.R.layout.simple_list_item_1, statistics
-        )
+    private fun setStatistics() {
+        statisticsList.add(StatisticsRecord("stat name", "value"))
+        statisticsList.add(StatisticsRecord("stat name that is long as hell", "value"))
+        statisticsList.add(StatisticsRecord("stat name that is even longer than hell", "long value"))
+        statisticsList.add(StatisticsRecord("stat name that is even longer than hell", "value"))
+
+        val adapter = StatisticsAdapter(this, R.layout.statistics_record_element, statisticsList)
         binding.lvStatisticsList.adapter = adapter
     }
+
+//      - - - DEPRECATED - - -
+//    private fun fillStatistics() {
+//        var statistics = arrayOf("Coins gained: 500", "Coins spent: 480", "Games played: 32", "Best strike: 7", "Total time spent: 35h", "Topics created: 0", "Average time per topic: 5 min", "Topics played: 80", "Topics rated: 77")
+//        val adapter: ArrayAdapter<String> = ArrayAdapter<String>(
+//            this,
+//            android.R.layout.simple_list_item_1, statistics
+//        )
+//        binding.lvStatisticsList.adapter = adapter
+//    }
 
     private fun actionBarSetup() {
         binding.actionBar.title.setText(R.string.profile_activity_title)
---
+++ src/application/MindGameApplication/app/src/main/java/anekdotas/mindgameapplication/java/StatisticsAdapter.kt
@@ -0,0 +1,34 @@
+package anekdotas.mindgameapplication.java
+
+import android.annotation.SuppressLint
+import android.content.Context
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.ArrayAdapter
+import android.widget.TextView
+import anekdotas.mindgameapplication.R
+
+class StatisticsAdapter (
+    private val myContext: Context, var myResource: Int, objects: ArrayList<StatisticsRecord>
+) :
+    ArrayAdapter<StatisticsRecord?>(myContext, myResource, objects as List<StatisticsRecord?>) {
+    @SuppressLint("ViewHolder")
+    override fun getView(position: Int, convertView: View?, parent: ViewGroup): View {
+
+//        var convertView = convertView
+        val statisticsName = getItem(position)!!.statisticsName
+        val statisticsValue = getItem(position)!!.statisticsValue
+
+        val inflater = LayoutInflater.from(myContext)
+        val convertView = inflater.inflate(myResource, parent, false)
+        val tvStatisticsName = convertView.findViewById<View>(R.id.tv_statisticsName) as TextView
+        val tvStatisticsValue = convertView.findViewById<View>(R.id.tv_statisticsValue) as TextView
+
+
+        tvStatisticsName.text = statisticsName
+        tvStatisticsValue.text = statisticsValue
+
+        return convertView
+    }
+}
